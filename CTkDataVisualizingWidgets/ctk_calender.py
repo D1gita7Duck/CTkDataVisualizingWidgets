29abb7f7e1cc042dcd4c7fb9cf112b65403ff274
D1gita7Duck <147811924+D1gita7Duck@users.noreply.github.com>
2024-12-17 22:09:19 +0530

    date is highlighted on mouse click two week formats changed go_to_today logic

--- CTkDataVisualizingWidgets/ctk_calender.py
+++ CTkDataVisualizingWidgets/ctk_calender.py
@@ -6,9 +6,10 @@ import tkinter as tk
 
 class CTkCalendar(ctk.CTkFrame):
     """
-    Calendar widget to display certain month, each day is rendered as Label.
+    Calendar widget to display certain month, each day is rendered as Button.\n
+    If you do not define today_fg_color, today_text_color and date_highlight_color it will be rendered as other days.\n
+    Default format for a week is Sun ... Sat. Can be changed to Mon ... Sun by passing calendar_monday_first=True\n
 
-    If you do not define today_fg_color and today_text_color it will be rendered as other days
     """
     def __init__(self, master,
                  today_fg_color=None,
@@ -21,6 +22,7 @@ class CTkCalendar(ctk.CTkFrame):
                  border_color=None,
                  bg_color="transparent",
                  background_corner_colors=None,
+                 date_highlight_color = None,
                  title_bar_fg_color=None,
                  title_bar_border_width=None,
                  title_bar_border_color=None,
@@ -40,6 +42,7 @@ class CTkCalendar(ctk.CTkFrame):
                  calendar_days_fg_color=None,
                  calendar_dates_state="normal",
                  calendar_dates_command=None,
+                 calendar_monday_first = False,
                  calendar_label_pad=1):
 
         super().__init__(master=master,
@@ -55,6 +58,8 @@ class CTkCalendar(ctk.CTkFrame):
         # data
         self.today_text_color = today_text_color
         self.today_fg_color = today_fg_color
+        self.date_highlight_color = date_highlight_color
+        self.highlighted_btn : ctk.CTkButton = None
         self.today = self.current_date()
         self.day, self.month, self.year = self.today[:]
         self.labels_by_date = dict()
@@ -84,6 +89,7 @@ class CTkCalendar(ctk.CTkFrame):
         self.calendar_dates_state = calendar_dates_state
         self.calendar_dates_command = calendar_dates_command
         self.calendar_label_pad = calendar_label_pad
+        self.calendar_monday_first = calendar_monday_first
 
         # creating header and calendar frames
         self.content_frame = ctk.CTkFrame(self, fg_color="transparent", width=width, height=height)
@@ -128,14 +134,55 @@ class CTkCalendar(ctk.CTkFrame):
         rows = tuple([i for i in range(len(current_month)+1)])
         calendar_frame.rowconfigure(rows, weight=1, uniform="b")
 
-        # labels for days of the week
-        self.setup_days_of_week_label(calendar_frame, row=0)
-
         # labels for dates
-        for row in range(len(current_month)):
-            for column in range(7):
-                if current_month[row][column] != 0:
-                    self.setup_label_normal(calendar_frame, current_month[row][column], row+1, column)
+        if self.calendar_monday_first:
+            # labels for days of the week
+            self.setup_days_of_week_label(calendar_frame, row=0, monday_first = True)
+
+            for row in range(len(current_month)):
+                for column in range(7):
+                    if current_month[row][column] != 0:
+                        self.setup_button_normal(calendar_frame, current_month[row][column], row+1, column)
+        else:
+            # labels for days of the week
+            self.setup_days_of_week_label(calendar_frame, row=0, monday_first = False)
+            formatted_month = [[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0]]
+            
+            for date_row in current_month[3:]:
+                for t in date_row:
+                    if (t == 0) : break
+                    max_month_date = t
+                else:
+                    continue
+                break
+            i=0
+            while (i<7):
+                if (current_month[0][i] == 1):
+                    break
+                i += 1
+            i = (i+1)%7
+
+            row = 0
+            column = i
+            date = 1
+            while (row<5):
+                column %= 7
+                while (column<7):
+                    if (date > max_month_date):
+                        break
+                    formatted_month[row][column] = date
+                    date += 1
+                    column += 1
+                else:
+                    row += 1
+                    continue
+                break
+
+            # print(formatted_month)
+            for row in range(len(formatted_month)):
+                for column in range(7):
+                    if formatted_month[row][column] != 0:
+                        self.setup_button_normal(calendar_frame, formatted_month[row][column], row+1, column)
 
         calendar_frame.place(relx=0.5, rely=0.97, anchor="s", relheight=0.75, relwidth=0.95)
 
@@ -156,6 +203,12 @@ class CTkCalendar(ctk.CTkFrame):
         self.create_calendar_frame()
     
     def go_to_today(self):
+        self.month, self.year = self.current_date()[1:]
+        self.month_label.set(calendar.month_name[self.month][0:3])
+        self.year_label.set(self.year)
+        self.create_calendar_frame()
+
+    def go_to_today2(self):
         today_month, today_year = self.current_date()[1:]
         # print(today_month, today_year, self.month, self.year, end=' ** ')
         del_month = today_month-self.month
@@ -173,30 +226,37 @@ class CTkCalendar(ctk.CTkFrame):
             return True
         return False
 
-    def on_date_click(self, btn_state, date):
+    def on_date_click(self, button : ctk.CTkButton, btn_state : str, date : int):
         if btn_state != "disabled":
+            if self.highlighted_btn is not None:
+                self.highlighted_btn.configure(fg_color = ctk.ThemeManager.theme["CTkButton"]["fg_color"])
+            button.configure(fg_color = self.date_highlight_color)
+            self.highlighted_btn = button
             if self.calendar_dates_command is not None:
                 self.calendar_dates_command(date, self.month, self.year)
 
     # creating normal date labels for normal calendar
-    def setup_label_normal(self, frame, day, row, column):
+    def setup_button_normal(self, frame, day, row, column):
         if self.today_fg_color is not None and self.date_is_today((day, self.month, self.year)):
-            ctk.CTkButton(frame, text=str(day), corner_radius=5,
+            btn = ctk.CTkButton(frame, text=str(day), corner_radius=5,
                          fg_color=self.today_fg_color, font=ctk.CTkFont("Arial", 11),
                          text_color=self.today_text_color, state=self.calendar_dates_state,
-                         command=lambda : self.on_date_click(self.calendar_dates_state, day)).grid(row=row, column=column, sticky="nsew",
-                                                                padx=self.calendar_label_pad,
-                                                                pady=self.calendar_label_pad)
+                         command=lambda : self.on_date_click(btn, self.calendar_dates_state, day))
+            btn.grid(row=row, column=column, sticky="nsew", padx=self.calendar_label_pad, pady=self.calendar_label_pad)
+            
+
         else:
-            ctk.CTkButton(frame, text=str(day), corner_radius=5,
+            btn = ctk.CTkButton(frame, text=str(day), corner_radius=5,
                          fg_color=self.calendar_text_fg_color, font=ctk.CTkFont("Arial", 11),
                          text_color=self.calendar_text_color, state=self.calendar_dates_state,
-                         command=lambda : self.on_date_click(self.calendar_dates_state, day)).grid(row=row, column=column, sticky="nsew",
-                                                                   padx=self.calendar_label_pad,
-                                                                   pady=self.calendar_label_pad)
+                         command=lambda : self.on_date_click(btn, self.calendar_dates_state, day))
+            btn.grid(row=row, column=column, sticky="nsew", padx=self.calendar_label_pad, pady=self.calendar_label_pad)
 
-    def setup_days_of_week_label(self, frame, row):
-        self.days_of_week = ("Mo", "Tu", "We", "Th", "Fr", "Sa", "Su")
+    def setup_days_of_week_label(self, frame, row : int, monday_first : bool):
+        if monday_first:
+            self.days_of_week = ("Mo", "Tu", "We", "Th", "Fr", "Sa", "Su")
+        else:
+            self.days_of_week = ("Su", "Mo", "Tu", "We", "Th", "Fr", "Sa")
         for column in range(7):
             ctk.CTkLabel(frame, text=str(self.days_of_week[column]), corner_radius=5,
                             fg_color=self.calendar_days_fg_color, font=ctk.CTkFont("Arial", 11),
